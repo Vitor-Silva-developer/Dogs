fcd32f153d3391351a6e60dfc029f27fc4849f7d
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-06 20:06:42 -0300

    created login/header/footer/home components and finished header layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
