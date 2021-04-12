93b110423d719eacbb88eac23de51dc39a35bb0e
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-12 11:13:33 -0300

    created sign up system and sign up page layout

--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import Input from "../Login/formComponents/Input/Input";
+import Button from "../Login/formComponents/Button/Button";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../Api/api";
+import { UserContext } from "../../UserContext";
 const SignUp = () => {
-    return (
-        <div>
-            sign up
-        </div>
-    )
-}
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+
+const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className={`title`}>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Username" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Password" type="password" name="password" {...password} />
+        <Button>Sign Up</Button>
+      </form>
+    </section>
+  );
+};
 
 export default SignUp;
