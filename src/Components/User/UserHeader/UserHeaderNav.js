826d79911dd3ac7b4346e6224b9aa3af1a3ea6eb
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-16 09:55:03 -0300

    Created mobile menu

--- src/Components/User/UserHeader/UserHeaderNav.js
+++ src/Components/User/UserHeader/UserHeaderNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../../UserContext";
 
 import { ReactComponent as MyPhotos } from "../../../Assets/feed.svg";
@@ -8,29 +8,43 @@ import { ReactComponent as AddPhoto } from "../../../Assets/adicionar.svg";
 import { ReactComponent as Logout } from "../../../Assets/sair.svg";
 
 import styles from "./UserHeaderNav.module.css";
+import useMedia from "../../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
-  const [mobile, setMobile] = React.useState(null);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const {pathname} = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname])
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/account" end activeClassName={styles.active}>
-        <MyPhotos /> {mobile && "My Photos"}
-      </NavLink>
-      <NavLink to="/account/statistics" activeClassName={styles.active}>
-        <Stats />
-        {mobile && "Statistics"}
-      </NavLink>
-      <NavLink to="/account/post" activeClassName={styles.active}>
-        <AddPhoto />
-        {mobile && "Add a photo"}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Logout />
-        {mobile && "Logout"}
-      </button>
-    </nav>
+    <>
+      {mobile && <button
+        aria-label="Menu"
+        className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+        onClick={() => setMobileMenu(!mobileMenu)}
+      ></button>}
+      <nav className={`${mobile? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}>
+        <NavLink to="/account" end activeClassName={styles.active}>
+          <MyPhotos /> {mobile && "My Photos"}
+        </NavLink>
+        <NavLink to="/account/statistics" activeClassName={styles.active}>
+          <Stats />
+          {mobile && "Statistics"}
+        </NavLink>
+        <NavLink to="/account/post" activeClassName={styles.active}>
+          <AddPhoto />
+          {mobile && "Add a photo"}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Logout />
+          {mobile && "Logout"}
+        </button>
+      </nav>
+    </>
   );
 };
 
