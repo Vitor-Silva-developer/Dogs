99c6ad09d356b193808f8d35f4e193fc424f479d
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-17 11:50:58 -0300

    finished Posting Photo page

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import Feed from "../Feed/Feed";
 import UserHeader from "./UserHeader/UserHeader";
-import UserPhotoPost from "./UserPhotoPost";
+import UserPhotoPost from "./UserPhotoPost/UserPhotoPost";
 import UserStats from "./UserStats";
 
 const User = () => {
-  return <section className="container">
+  return (
+    <section className="container">
       <UserHeader />
       <Routes>
-          <Route path="/" element={<Feed />}/>
-          <Route path="/post" element={<UserPhotoPost />}/>
-          <Route path="/statistics" element={<UserStats />}/>
+        <Route path="/" element={<Feed />} />
+        <Route path="/post" element={<UserPhotoPost />} />
+        <Route path="/statistics" element={<UserStats />} />
       </Routes>
-  </section>;
+    </section>
+  );
 };
 
 export default User;
