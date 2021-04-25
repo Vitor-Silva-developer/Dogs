1c5c70d95ed9e26d49bdbd9160a8a4d615867611
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-25 19:03:32 -0300

    created the statistics page

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Head from '../Helper/Head'
+import React from "react";
+import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/error";
+import { GET_STATS } from "../../Api/api";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Statistics" />
-            stats
-        </div>
-    )
-}
+  const { data, error, loading, request } = useFetch();
 
-export default UserStats
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Statistics" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
+};
+
+export default UserStats;
